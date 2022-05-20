7f31bcc4898912fee2b3f31636678e001ab76077
coofzilla <jeric614@yahoo.com>
2022-05-20 10:59:49 +0900

    integrate marketing into container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  return <h1>Hi there!</h1>;
+  return (
+    <div>
+      <h1>Hi container</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 };
